e24613f2edc987186f80c9e8d62b9f9a7595547e
Pradip Bhatt <bhattsammar04@gmail.com>
2024-06-27 21:03:57 +0545

    Upload form fixed

--- src/components/UploadForm.jsx
+++ src/components/UploadForm.jsx
@@ -1,20 +1,28 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const UploadForm = ({ getPdf }) => {
+  const [title, setTitle] = useState("");
   const [file, setFile] = useState(null);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData();
+    formData.append("title", title);
     formData.append("file", file);
 
     try {
-      await axios.post("https://soe-notes-pdf-backend.onrender.com/upload", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
-      getPdf();
+      const result = await axios.post(
+        "https://soe-notes-pdf-backend.onrender.com/upload-files",
+        formData,
+        {
+          headers: { "Content-Type": "multipart/form-data" },
+        }
+      );
+      if (result.data.status === "ok") {
+        alert("Uploaded Successfully!!!");
+        getPdf();
+      }
     } catch (error) {
       console.error("Error uploading file:", error);
     }
@@ -25,7 +33,16 @@ const UploadForm = ({ getPdf }) => {
       <h2 className="text-2xl font-bold mb-4">Upload PDF</h2>
       <form onSubmit={handleSubmit}>
         <input
+          type="text"
+          placeholder="Title"
+          required
+          onChange={(e) => setTitle(e.target.value)}
+          className="mb-4 p-2 w-full border rounded"
+        />
+        <input
           type="file"
+          accept="application/pdf"
+          required
           onChange={(e) => setFile(e.target.files[0])}
           className="mb-4 p-2 w-full border rounded"
         />
