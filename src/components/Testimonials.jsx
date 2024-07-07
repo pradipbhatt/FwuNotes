f676ccbe05ada075272e5b530340999b895e6ddd
Pradip Bhatt <bhattsammar04@gmail.com>
2024-07-07 15:30:42 +0545

    testimonials enhanced

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const Testimonials = () => {
   const testimonials = [
@@ -6,16 +9,16 @@ const Testimonials = () => {
       name: 'Pradip Bhatt',
       title: 'CSE Student & Backend Developer',
       company: 'Far Western University',
-      img: 'https://pradipbhatt.com.np/medias/parry.jpg', // Replace with actual image URL
+      img: 'https://pradipbhatt.com.np/medias/parry.jpg',
       quoteTitle: 'Dedicated Developer',
       quote: 'SOE Notes is a testament to our commitment to excellence and innovation in education.',
       rating: 5
     },
     {
       name: 'Jelina Bhatt',
-      title: 'CSE Student',
+      title: 'CSE Student 4th Batch',
       company: 'Far Western University',
-      img: 'https://scontent.fktm1-1.fna.fbcdn.net/v/t39.30808-6/291759961_587583433092843_1552650560748294747_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=86c6b0&_nc_ohc=jk2bvqPuHsQQ7kNvgHg60Bc&_nc_ht=scontent.fktm1-1.fna&gid=A_S5Xw_pQapORxRIjiu0SXU&oh=00_AYBjN3x7PHr0une2QYBy9-adrXjDlm4H7qY8_-VsOaXyGA&oe=668B591B', // Replace with actual image URL
+      img: 'https://scontent.fktm1-1.fna.fbcdn.net/v/t39.30808-6/291759961_587583433092843_1552650560748294747_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=86c6b0&_nc_ohc=jk2bvqPuHsQQ7kNvgHg60Bc&_nc_ht=scontent.fktm1-1.fna&gid=A_S5Xw_pQapORxRIjiu0SXU&oh=00_AYBjN3x7PHr0une2QYBy9-adrXjDlm4H7qY8_-VsOaXyGA&oe=668B591B',
       quoteTitle: 'Resourceful Support',
       quote: 'SOE Notes brings a new perspective to comparing educational institutions and providing essential resources.',
       rating: 4
@@ -24,25 +27,65 @@ const Testimonials = () => {
       name: 'Bijay Saud',
       title: 'CSE Student',
       company: 'Far Western University',
-      img: 'https://scontent.fktm1-1.fna.fbcdn.net/v/t39.30808-6/341151557_101011719647949_7538820499509787011_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=tIWHbf3-jPcQ7kNvgHg6z3Z&_nc_ht=scontent.fktm1-1.fna&oh=00_AYBiSGMcJ8MK8Uqzdaxk2yXl10ES95S68s66IBjgQA1qxg&oe=668B7696', // Replace with actual image URL
+      img: 'https://scontent.fktm1-1.fna.fbcdn.net/v/t39.30808-6/341151557_101011719647949_7538820499509787011_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=tIWHbf3-jPcQ7kNvgHg6z3Z&_nc_ht=scontent.fktm1-1.fna&oh=00_AYBiSGMcJ8MK8Uqzdaxk2yXl10ES95S68s66IBjgQA1qxg&oe=668B7696',
       quoteTitle: 'Handwritten Notes Provider',
       quote: 'The handwritten notes provided by Bijay have been crucial in supporting our education.',
+      rating: 5
+    },
+    {
+      name: 'Mukul Bhatt',
+      title: 'CSE Student',
+      company: 'Far Western University',
+      img: '../../public/mukul.jpeg', // Replace with actual image URL
+      quoteTitle: 'Handwritten Notes Provider Second Batch',
+      quote: 'The handwritten notes provided by Mukul have been crucial in supporting our education.',
       rating: 5
     }
   ];
 
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 1000, // Slide every 1 second
+    slidesToShow: 3, // Show 3 cards at a time
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000, // Auto slide every 2 seconds
+    responsive: [
+      {
+        breakpoint: 1024, // lg breakpoint
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768, // md breakpoint
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 0,
+          infinite: true,
+          dots: true
+        }
+      }
+    ]
+  };
+
   return (
     <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 py-20">
       <h1 className="text-3xl font-serif text-center mb-12">Testimonials</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+      <Slider {...sliderSettings} className="mx-auto max-w-5xl">
         {testimonials.map((testimonial, index) => (
-          <div key={index} className="bg-white rounded-lg p-6 shadow-md">
+          <div key={index} className="bg-white rounded-lg p-6 shadow-md mx-4">
             <img
               src={testimonial.img}
               alt={testimonial.name}
-              className="w-16 h-16 rounded-full mx-auto"
+              className="w-24 h-24 rounded-full mx-auto mb-6 object-cover"
             />
-            <div className="text-center mt-6">
+            <div className="text-center">
               <h4 className="text-lg font-bold text-gray-800">{testimonial.name}</h4>
               <p className="text-sm text-gray-500 mb-2">{testimonial.title} | {testimonial.company}</p>
               <h2 className="italic text-lg font-bold text-gray-800 mb-2">{testimonial.quoteTitle}</h2>
@@ -62,7 +105,7 @@ const Testimonials = () => {
             </div>
           </div>
         ))}
-      </div>
+      </Slider>
     </div>
   );
 };
